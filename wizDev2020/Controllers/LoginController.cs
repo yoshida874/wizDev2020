3854d269fe1002925958aa2ddecb64e3e06379c1
yoshida874 <ud8mnzdd@gmail.com>
2020-11-27 14:34:55 +0900

    不要なインポートを削除

--- wizDev2020/Controllers/LoginController.cs
+++ wizDev2020/Controllers/LoginController.cs
@@ -2,9 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using wizDev2020.Models;
 using System.Security.Claims;
