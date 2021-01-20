b2c33a14215920b5680d9ec74f1aa8de94733483
yoshida874 <45518040+yoshida874@users.noreply.github.com>
2021-01-20 23:06:25 +0900

    Revert "Revert "位置情報取得、ルート検索など""

--- wizDev2020/Startup.cs
+++ wizDev2020/Startup.cs
@@ -76,7 +76,7 @@ namespace wizDev2020
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=login}/{action=Index}/{id?}");
+                    pattern: "{controller=home}/{action=Index}/{id?}");
             });
         }
     }
